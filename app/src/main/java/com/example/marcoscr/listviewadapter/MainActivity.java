ae1c8e22d4f5846632cf2301747075d9e6e79280
Marcos Puc <darcos_77.94@hotmail.com>
2015-07-06 02:16:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/marcoscr/listviewadapter/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.marcoscr.listviewadapter;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class MyActivity extends Activity {
+
+    ListViewAdapter adapter;
+
+    String[] titulo = new String[]{
+            "titulo1",
+            "titulo2",
+            "titulo3",
+            "titulo4",
+    };
+
+    int[] imagenes = {
+            R.drawable.icon01,
+            R.drawable.icon02,
+            R.drawable.icon01,
+            R.drawable.icon02
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ListView lista = (ListView) findViewById(R.id.listView1);
+        adapter = new ListViewAdapter(this, titulo, imagenes);
+        lista.setAdapter(adapter);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int 1, long 1) {
+
+                Toast.makeText(getApplicationContext(),"posición " + 1, Toast.LENGTH_SHORT).show());
+
+                lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                        Toast.makeText(getApplicationContext(), "posición mas largo " + (1+1), Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                });
+
+            }
+        });
+
+
+    }
+}
